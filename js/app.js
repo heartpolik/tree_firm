24c50cc1aadc6f525843b20968a36bf0df25fcbf
Constantine Polischuk <heart.polik@gmail.com>
2016-05-17 03:57:15 +0300

    almoust done interface

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
   var newOrgNameDom = document.getElementById('new-name');
   var newOrgCashDom = document.getElementById('new-cash');
   var newOrgParentDom = document.getElementById('new-parent');
-  var newParentId = 0;
+  var newParentId = document.getElementById('new-pid');
   var syncDom = document.getElementById('sync-wrapper');
 
   // EDITING STARTS HERE (you dont need to edit anything above this line)
@@ -20,37 +20,45 @@
   db.changes({
     since: 'now',
     live: true
-  }).on('change', showTodos);
+  }).on('change', showFirms);
 
   // We have to create a new todo document and enter it in the database
-  function addTodo(name, money, parent) {
+  function addFirm(name, money, parent) {
     var firm = {
       _id: new Date().toISOString(),
       title: name,
       cash: money,
       parent_id: parent,
-     // completed: false
+
     };
 
     db.put(firm, function callback(err, result) {
       if (!err) {
-        console.log('Successfully posted a todo!');
+        console.log('Successfully posted a firm!');
       }
     });
   }
 
-  // Show the current list of todos by reading them from the database
-  function showTodos() {
-    db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-      redrawTodosUI(doc.rows);
-    });
+  // Show the current list of firms by reading them from the database
+  function showFirms()
+  {
+    db.allDocs
+    (
+        {
+          include_docs: true,
+          descending: true
+        },
+          function(err, doc)
+        {
+          redrawFirmUI(doc.rows);
+        }
+    );
   }
 
-  function checkboxChanged(firm, event) {
-  //  firm.completed = event.target.checked;
+  function checkboxChanged(firm)
+  {
     newOrgParentDom.value = firm.title;
-    newParentId = firm.id;
-   // db.put(firm);
+    newParentId.value = firm._id;
   }
 
   // User pressed the delete button for a todo, delete it
@@ -58,16 +66,26 @@
     db.remove(firm);
   }
 
-  // The input box when editing a todo has blurred, we should save
-  // the new title or delete the todo if the title is empty
-  function todoBlurred(firm, event) {
+  // The input box when editing a firm has blurred, we should save
+  // the new title
+  function firmBlurred(firm, event) {
     var trimmedText = event.target.value.trim();
     if (!trimmedText) {
-      db.remove(firm);
+      firm.title = 'Unnamed';
     } else {
       firm.title = trimmedText;
-      db.put(firm);
     }
+    db.put(firm);
+  }
+
+  function moneyBlurred(firm, event) {
+    var trimmedText = event.target.value.trim();
+    if (!trimmedText) {
+      firm.cash = 0;
+    } else {
+      firm.cash = trimmedText;
+    }
+    db.put(firm);
   }
 
   // Initialise a sync with the remote server
@@ -86,25 +104,38 @@
   }
 
   // User has double clicked a todo, display an input so they can edit the title
-  function todoDblClicked(firm, par) {
+  function firmDblClicked(firm, par) {
     var div = document.getElementById('li_' + firm._id);
     var inputEditTodo = document.getElementById('input_' + firm._id);
     div.className = 'editing';
     inputEditTodo.focus();
   }
 
+  function cashDblClicked(firm, par) {
+    var div = document.getElementById('li_' + firm._id);
+    var inputEditTodo = document.getElementById('input_c' + firm._id);
+    div.className = 'editing';
+    inputEditTodo.focus();
+  }
+
   // If they press enter while editing an entry, blur it to trigger save
   // (or delete)
-  function todoKeyPressed(firm, event) {
+  function firmKeyPressed(firm, event) {
     if (event.keyCode === ENTER_KEY) {
       var inputEditTodo = document.getElementById('input_' + firm._id);
       inputEditTodo.blur();
     }
   }
+  function moneyKeyPressed(firm, event) {
+    if (event.keyCode === ENTER_KEY) {
+      var inputEditTodo = document.getElementById('input_c' + firm._id);
+      inputEditTodo.blur();
+    }
+  }
 
-  // Given an object representing a todo, this will create a list item
+  // Given an object representing a firm, this will create a list item
   // to display it.
-  function createTodoListItem(firm) {
+  function createFirmListItem(firm) {
     var checkbox = document.createElement('input');
     checkbox.className = 'toggle';
     checkbox.type = 'checkbox';
@@ -112,15 +143,14 @@
 
     var lName = document.createElement('label');
     lName.appendChild( document.createTextNode(firm.title));
-    lName.addEventListener('dblclick', todoDblClicked.bind(this, firm));
+    lName.addEventListener('dblclick', firmDblClicked.bind(this, firm));
 
     var lCash = document.createElement('label');
     lCash.appendChild( document.createTextNode(firm.cash));
-    lCash.addEventListener('dblclick', todoDblClicked.bind(this, firm));
+    lCash.addEventListener('dblclick', cashDblClicked.bind(this, firm));
 
     var lParent = document.createElement('label');
     lParent.appendChild( document.createTextNode(firm.parent_id));
-    lParent.addEventListener('dblclick', todoDblClicked.bind(this, firm));
 
     var deleteLink = document.createElement('button');
     deleteLink.className = 'destroy';
@@ -138,45 +168,53 @@
     inputEditTodo.id = 'input_' + firm._id;
     inputEditTodo.className = 'edit';
     inputEditTodo.value = firm.title;
-    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, firm));
-    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, firm));
+    inputEditTodo.addEventListener('keypress', firmKeyPressed.bind(this, firm));
+    inputEditTodo.addEventListener('blur', firmBlurred.bind(this, firm));
+
+    var inputEditMoney = document.createElement('input');
+    inputEditMoney.type = 'number';
+    inputEditMoney.id = 'input_c' + firm._id;
+    inputEditMoney.className = 'edit';
+    inputEditMoney.value = firm.cash;
+    inputEditMoney.addEventListener('keypress', moneyKeyPressed.bind(this, firm));
+    inputEditMoney.addEventListener('blur', moneyBlurred.bind(this, firm));
 
     var li = document.createElement('li');
     li.id = 'li_' + firm._id;
     li.appendChild(divDisplay);
     li.appendChild(inputEditTodo);
-
-    if (firm.completed) {
-      li.className += 'complete';
-      checkbox.checked = true;
-    }
+    li.appendChild(inputEditMoney);
 
     return li;
   }
 
-  function redrawTodosUI(todos) {
+  function redrawFirmUI(todos) {
     var ul = document.getElementById('todo-list');
     ul.innerHTML = '';
     todos.forEach(function(firm) {
-      ul.appendChild(createTodoListItem(firm.doc));
+      
+      ul.appendChild(createFirmListItem(firm.doc));
     });
   }
 
-  function newTodoKeyPressHandler() {
-
-      addTodo(newOrgNameDom.value, newOrgCashDom.value, newOrgParentDom.value);
-      newOrgNameDom.value = '';
-     /* newOrgCashDom.value = '';
-*/      newOrgParentDom.value = '';
+  function newFirmKeyPressHandler() {
+    if (newOrgNameDom.value && newOrgCashDom.value){
+      addFirm(newOrgNameDom.value, newOrgCashDom.value, newParentId.value);
+        newOrgNameDom.value = '';
+        newOrgCashDom.value = '';
+        newOrgParentDom.value = '';
+        newParentId.value = '';
+    } else alert("Please, fill empty fields!");
 
   }
 
-  function addEventListeners() {
-    newButton.addEventListener('click', newTodoKeyPressHandler, false);
+  function addEventListeners()
+  {
+    newButton.addEventListener('click', newFirmKeyPressHandler, false);
   }
 
   addEventListeners();
-  showTodos();
+  showFirms();
 
   if (remoteCouch) {
     sync();
